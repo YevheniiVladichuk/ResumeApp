edbb659f691be8ff2d700ac819e1bec3366ad1ad
YevheniiVladichuk <gorillakong@icloud.com>
2022-08-24 22:42:40 +0300

    Added ability to sign up new users with Firebase

--- ResumeApp/ViewControllers/SignUpViewController.swift
+++ ResumeApp/ViewControllers/SignUpViewController.swift
@@ -6,8 +6,12 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseFirestore
 
 class SignUpViewController: UIViewController {
+    @IBOutlet var mainStackView: UIStackView!
+    
     @IBOutlet var firstNameField: UITextField!
     @IBOutlet var lastNameField: UITextField!
     @IBOutlet var emailField: UITextField!
@@ -19,7 +23,6 @@ class SignUpViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         stylizeElements()
-
     }
     
     //keyboard disappear by tap anywhere view
@@ -28,7 +31,10 @@ class SignUpViewController: UIViewController {
     }
     
     @IBAction func signUpButtonTapped(_ sender: UIButton) {
-      
+        self.view.endEditing(true)
+        let error = checkFields()
+        guard error == nil else {return showError(error: error!)}
+        creatNewUser()
     }
     
     
@@ -50,6 +56,7 @@ class SignUpViewController: UIViewController {
         addPlaceholder()
     }
     
+    
     //add placeholder with opacity
     func addPlaceholder() {
         firstNameField.attributedPlaceholder = NSAttributedString(string: "First name", attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray.withAlphaComponent(0.5)])
@@ -65,6 +72,43 @@ class SignUpViewController: UIViewController {
         view?.window?.makeKeyAndVisible()
     }
     
+    //check the fields
+    func checkFields()-> String? {
+        if firstNameField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || lastNameField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            emailField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            passwordField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+            return "Sorry, you need to fil all fields !"
+        }
+        
+     return nil
+    }
     
+    //show error message
+    func showError(error: String) {
+        errorLabel.alpha = 1
+        errorLabel.text! = error
+    }
+    
+    //creat new user
+    func creatNewUser() {
+        Auth.auth().createUser(withEmail: emailField.text!, password: passwordField.text!) { authResult, error in
+            if error != nil {
+                self.showError(error: error!.localizedDescription)
+            }
+        }
+        segue(id: "LoginVC")
+    }
+    
+    //change stack
+    func newStack() {
+        mainStackView.isMultipleTouchEnabled = false
+        mainStackView.alpha = 0
+    }
+    
+    //нужно добавить новый лейбл и кнопку логи после регистрации на этом же вью так же убрать кнопу уже есть аккаунт
+    
+
     
 }
+
+
